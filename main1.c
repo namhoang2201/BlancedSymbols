0596ff44a5c6d4bf7b4bcc8746af23a2e24cc52e
namhoang2201 <hoangnamtb95@gmail.com>
2018-04-06 04:04:46 +0700

    Declare functions to handle the problem

--- main1.c
+++ main1.c
@@ -3,6 +3,21 @@
 #include<stdlib.h>
 #include "stack.h"
 
+_Bool isTerm(char c){
+
+};
+
+_Bool isOpenTer(){
+	
+}
+
+_Bool matches(char x, char y){
+	
+}
+
+_Bool isBalanced(char xau){
+	
+}
 int main(){
 	
 }
