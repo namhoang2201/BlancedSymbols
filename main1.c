b5b99a5be948e170c08a931ae2f19c93695d274f
namhoang2201 <hoangnamtb95@gmail.com>
2018-04-06 09:17:36 +0700

    modify return value of functions because C99 and that C90 does not support the boolean data type

--- main1.c
+++ main1.c
@@ -1,31 +1,32 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdbool.h>
 #include<stdlib.h>
 #include "stack.h"
 
 char Terms[3][2] = {{'(',')'},{'[',']'},{'{','}'}};
 
-_Bool isTerm(char c){
+bool isTerm(char c){
 	int i,j;
 	for(i=0; i<3; i++){
 		for(j=0; j<2; j++){
 				if(Terms[i][j] ==c ){
-				return 1;
+				return true;
 			}
 		}
 	}
-	return 0;
+	return false;
 };
 
-_Bool isOpenTerm(){
+bool isOpenTerm(){
 	
 }
 
-_Bool matches(char x, char y){
+bool matches(char x, char y){
 	
 }
 
-_Bool isBalanced(char xau){
+bool isBalanced(char xau){
 	
 }
 int main(){
