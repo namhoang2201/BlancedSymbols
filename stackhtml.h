e6fadbac72ee502f51d0b23eb7f945fbb97bec45
namhoang2201 <hoangnamtb95@gmail.com>
2018-04-20 00:48:10 +0700

    Define a stackNodeHtml in stackHtml.h by using linked list.

---
+++ stackhtml.h
@@ -0,0 +1,7 @@
+// Dinh nghia stack dung danh sach lien ket don
+// Voi truong du lieu cua 1 nut la mot chuoi ky tu
+
+typedef struct stackNodeHtml{
+	char item[10];
+	struct stackNodeHtml *next;
+};
